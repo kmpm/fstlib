88314374ee4b9adfc6132ba920a926ea09a0d17a
Peter M <kmpm@birchroad.net>
2009-07-20 11:29:10 +0200

    more endian testing

--- lib/fstlib/easyip/packet.rb
+++ lib/fstlib/easyip/packet.rb
@@ -3,19 +3,19 @@ require 'bit-struct'
 module EasyIP
   
   class Header < BitStruct
-    unsigned  :flags, 8
-    unsigned  :error, 8
-    unsigned  :counter, 16
-    unsigned  :index, 16
-    unsigned  :spare1, 8
-    unsigned  :send_type, 8
-    unsigned  :send_size, 16
-    unsigned  :send_offset, 16
-    unsigned  :spare2, 8
-    unsigned  :req_type, 8
-    unsigned  :req_size, 16
-    unsigned  :req_offset_server, 16
-    unsigned  :req_offset_client, 16
+    unsigned  :flags,               8 #EasyIP::Flags
+    unsigned  :error,               8
+    unsigned  :counter,             16, :endian => :little
+    unsigned  :index,               16
+    unsigned  :spare1,              8,  :endian => :little
+    unsigned  :send_type,           8 #EasyIP::Operand
+    unsigned  :send_size,           16, :endian => :little
+    unsigned  :send_offset,         16, :endian => :little
+    unsigned  :spare2,              8
+    unsigned  :req_type,            8 #EasyIP::Operand
+    unsigned  :req_size,            16, :endian => :little
+    unsigned  :req_offset_server,   16, :endian => :little
+    unsigned  :req_offset_client,   16, :endian => :little
     rest :data
   end
   
--- test/test.rb
+++ test/test.rb
@@ -1,24 +1,29 @@
 require "test/unit"
 require "fstlib"
 class Easy < Test::Unit::TestCase
-  COUNTER_START = 20
+  COUNTER_START = 5
   
-  def test_basics
+  def test_header
     
-    a = EasyIP::Header.new
-    a.flags=EasyIP::Flags::RESP
-    a.counter = COUNTER_START
-    a.send_type=EasyIP::Operand::STRING
-    a.send_offset=10
+    header = EasyIP::Header.new
+    header.flags=EasyIP::Flags::RESP
+    header.error=3
+    header.counter = 5
+    header.send_type=EasyIP::Operand::INPUT_WORD
+    header.send_size=0xBBCC
+    header.send_offset=0xAAEE
     
     # puts EasyIP::Header.describe
     # puts a.inspect
     
     assert_equal 20*8, EasyIP::Header.bit_length
-    assert_equal 128, a.flags
-    assert_equal COUNTER_START, a.counter
     
-    puts a.unpack("H*") #.first.scan(/\d{8,8}/)
+    assert_equal EasyIP::Flags::RESP, header[0]
+    assert_equal 3, header[1]
+    assert_equal 5, header[2], 'wrong counter LSB'
+    assert_equal 0, header[3], 'wrong counter MSB'
+    
+    puts header.unpack("H*") #.first.scan(/\d{8,8}/)
 
   
   end
