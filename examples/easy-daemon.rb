636d9f8e6e99a5872303aba677423f50b0f39c0a
Peter M <kmpm@birchroad.net>
2009-07-21 11:50:12 +0200

    standalone udp easy-ip example

--- examples/easy-daemon.rb
+++ examples/easy-daemon.rb
@@ -1,5 +1,6 @@
 #!/usr/bin/ruby
 require "fstlib"
+
 require "socket"
 
 
@@ -15,8 +16,24 @@ if __FILE__ == $0
   while 1
    
         message, address = s.recvfrom(8192)
-        puts "Got data from", address
-        s.send(message, address)
+        request = EasyIP::Header.new(message)
+        puts "Got data from", address[3]
+        resp = EasyIP::Header.new
+        resp.counter=request.counter
+        resp.flags = EasyIP::Flags::RESP
+          
+        if request.req_type != EasyIP::Operand::EMPTY
+          resp.req_type = request.req_type
+          resp.req_offset_server = request.req_offset_server
+          resp.req_offset_client = request.req_offset_client
+          resp.req_size = request.req_size
+          resp.payload = [1,2,3,4,5,6,7,8,9,10]
+        end
+        puts request.inspect
+        puts resp.unpack("H*")
+        
+        s.send(resp, 0, address[3], address[1])
+        
    
   end
 end
