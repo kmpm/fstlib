89e7797d73c6e6c0b324cb5bb942da0f30af63b8
Peter Magnusson <peter.magnusson@mustad.se>
2010-07-07 14:36:15 +0200

    setup

---
+++ src/setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='FSTLib',
+		version='1.0',
+		author_email='peter@birchroad.net',
+		packages=['fstlib', 'fstlib.protocols'],
+		
+      )
